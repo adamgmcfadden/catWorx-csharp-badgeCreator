fdd11d135a108f237dde4a11017c4ef9efc7e5e5
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-11-15 13:36:38 -0500

    cli collecting all employee information and printing formatted data to the CLI

--- Program.cs
+++ Program.cs
@@ -14,14 +14,24 @@ namespace CatWorx.BadgeMaker
       {
         Console.WriteLine("Please enter a name: (leave empty to exit)");
          // ask user for input
-        string input = Console.ReadLine();
-        if (input == "")
+        string firstName = Console.ReadLine();
+        if (firstName == "")
         {
           // Break if the user hits ENTER without typing a name
           break;
         }
+
+        // add a Console.readline for each value
+        Console.Write("Enter last name: ");
+        string lastName = Console.ReadLine();
+
+        Console.Write("Enter employee ID: ");
+        int id = Int32.Parse(Console.ReadLine());
+
+        Console.Write("Enter photo URL: ");
+        string photoUrl = Console.ReadLine();
         // Create a new employee instance
-        Employee currentEmployee = new Employee(input, "McFadden");
+        Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
         // add employee (userInput) to employees List
         employees.Add(currentEmployee);
       }
@@ -29,10 +39,11 @@ namespace CatWorx.BadgeMaker
     }
     static void PrintEmployees(List<Employee> employees)
     {
-      for (int i = 0; i < employees.Count; i++)
-      {
-        Console.WriteLine(employees[i].GetName());
-      }
+      for (int i = 0; i < employees.Count; i++) 
+    {
+      string template = "{0,-10}\t{1,-20}\t{2}";
+      Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
+    }
     }
     static void Main(string[] args) // entry point
     {
